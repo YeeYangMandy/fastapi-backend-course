4e7d431ec61954f15886965a7c7f5f27dd286e0c
Yi Yang <oprtigk@gmail.com>
2024-10-24 16:01:38 +0800

    BaseModel

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Item(BaseModel):
+    name: str
+    price: float
+    description: str = None
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-@app.get("/test")
+@app.get("/test") #網址輸入特定路徑
 def read_test():
     return 'I love you!!'
+
+@app.post("/items/")
+def create_item(item: Item):
+    print(f"Received item: {item}")
+    return {"message": "Item received", "item": item}
