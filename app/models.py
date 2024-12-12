69fead6afd411554a4b11ccbdea0a834a551da68
jonathanXDD <jonathan27.tw@gmail.com>
2024-12-12 15:32:30 +0800

    a gift

--- app/models.py
+++ app/models.py
@@ -8,3 +8,10 @@ class Todo(Base):
     description = Column(String, nullable=True)
     completed = Column(Boolean, default=False)
     due_date = Column(Date, nullable=True)
+
+class User(Base):
+    __tablename__='users'
+    id=Column(Integer, primary_key = True, index = True)
+    username=Column(String(100), nullable=False)
+    password=Column(Integer,nullable=False)
+    email=Column(String(100),nullable=True)
